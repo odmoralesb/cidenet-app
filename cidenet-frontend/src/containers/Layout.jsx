acb04e996cd71e92fd91128a85c70306934a1561
dmawesi <david.morales@mawesi.net>
2021-07-31 21:56:28 -0500

    Se quitan escenario de pruebas en back y se agrega menu de navegacion en el front

--- cidenet-frontend/src/containers/Layout.jsx
+++ cidenet-frontend/src/containers/Layout.jsx
@@ -38,7 +38,51 @@ class Layout extends Component {
 
         return (
             <div>
-                <div className="contenido">{this.props.children}</div>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            CIDENET
+                        </a>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a
+                                        class="nav-link active"
+                                        aria-current="page"
+                                        href="#"
+                                    >
+                                        Home
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        Registtrar
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        Actualizar
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="container-fluid mt-3">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
